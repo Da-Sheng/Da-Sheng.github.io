aff7b4299ed20a358dca4ce6a4b16f96ec04e50e
盛旷 <1342143188@qq.com>
2019-05-05 11:43:36 +0800

    no message

--- dragger/static.js
+++ dragger/static.js
@@ -2,7 +2,7 @@ var hash = '44b8f079657169ec065d';
 var min = 'min.';
 
 // 每次更新lib为防止缓存，加版本号
-requireOwnJs('/lib/common.js?version=1.1.4');
+requireOwnJs('lib/common.js?version=1.1.4');
 
 setTimeout(function(){
   requireOwnJs('bundle.'+ min + hash +'.js');
